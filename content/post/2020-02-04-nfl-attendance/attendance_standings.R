7e3613ff75ac19fde335fbef4c4479f0abc6a591
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-05-05 19:50:45 +0100

    a not entirely terrible plot

--- content/post/2020-02-04-nfl-attendance/attendance_standings.R
+++
@@ -1,6 +0,0 @@
-attendance <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/attendance.csv')
-standings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/standings.csv')
-
-attendance_standings <- dplyr::left_join(attendance, standings, by = c("year", "team_name", "team"))
-
-write_rds_bak(attendance_standings, here::here("content", "post", "2020-02-04-nfl-attendance", "attendance_standings.rds"))
