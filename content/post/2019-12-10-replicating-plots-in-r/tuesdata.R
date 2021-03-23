392f9218c0d6ca43d22039651024f5bfaf9cac8b
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-03-23 17:34:37 +0000

    add replicating plots tt draft

--- content/post/2019-12-10-replicating-plots-in-r/tuesdata.R
+++
@@ -1,5 +0,0 @@
-
-tuesdata <- tidytuesdayR::tt_load("2019-12-10")
-tuesdata <- tidytuesdayR::tt_load(2019, week = 50)
-
-write_rds_bak(tuesdata, here::here("content", "post", "2019-12-10-replicating-plots-in-r", "tuesdata.rds"))
